86bd0e19df20058b1c3a26b39f02ea3ec239f1af
Andrei Hetmanski <drehetm@gmail.com>
2026-08-14 10:52:32 +0300

    Replace docker-compose with newest version usage

--- admin_run_tests.zsh
+++ admin_run_tests.zsh
@@ -1,3 +1,4 @@
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml run --rm test
-docker-compose -f docker-compose.test.yml down
+docker compose -f docker-compose.test.yml build
+docker compose -f docker-compose.test.yml run --rm test
+docker compose -f docker-compose.test.yml down
+
